ebebe1afdc0b4ee7dcbdc2e63c4b4b8666497b6e
Ruoran Wang <ruoran0502@gmail.com>
2017-12-16 21:10:11 -0800

    Add Readme file. Get files validated.

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Neighborhood Map
+
+This project shows a google map showing places at downtown Los Angeles. It's based on MVVM structure. It used knockout framework to connect the view and javascript. It also used Foursquare API and google map API and streetview to show the infomation of one place. When the user clicked on the markers, the infowindow opened to show the photo and Foursquare info of the place. It is the same if the user clicked on one place at the list view. There is a filter at the list view where the user can search a specific place by its name. The list view is toggled by a hamburger logo. The list showed when hover over the logo.
+
+The map view can be adjusted according to different device including phone , ipad and desktop.
+
+# Getting Started
+
+## Requirements
+
+- Users must have any a Web Browser
+
+### Steps To Run The Project
+
+1. Download the repository from Github.
+2. Go to the project directory.
+3. Drag **index.html** file into browser.
+4. The the peoject will be run on the browser.
+
+This project is a part of FSND (**Udacity**)
--- js/main.js
+++ js/main.js
@@ -1,128 +1,114 @@
+// create a global variable to hold the map
 var map;
-
+// model includes the place name,its Foursquare venue id and location
 var model = [
-{title:'Staples Center', venue_id:'42893400f964a5207e231fe3', location:{lat:34.043006, lng:-118.26736}},
-{title:'University of Southern California', venue_id:'45001adcf964a520ab381fe3', location:{lat:34.022352, lng:-118.285117}},
-{title:'China Town', venue_id:'508760b5e4b0d647dab288a6', location:{lat:34.062334, lng:-118.238331}},
-{title:'Pershing Square', venue_id:'4352e980f964a520c7281fe3', location:{lat:34.048569, lng:-118.252892}},
-{title:'Union Station', venue_id:'45b88de0f964a520ce411fe3', location:{lat:34.056219, lng:-118.236502}},
-{title:'Grand Park LA', venue_id:'4fecf601067d351381ea64fa', location:{lat:34.056329, lng:-118.246771}},
-{title:'Dodger Stadium', venue_id:'40db6b00f964a5207d011fe3', location:{lat:34.073851, lng:-118.239958}},
-{title:'Little Tokyo', venue_id:'41be2d00f964a520841e1fe3', location:{lat:34.052339, lng:-118.239505}},
-{title:'The Broad', venue_id:'4eeb4c14b634b469c36a8c80', location:{lat:34.054469, lng:-118.250558}},
-{title:'Los Angeles City Hall', venue_id:'4b5113edf964a520314127e3', location:{lat:34.053527, lng:-118.242932}}
-];
-
+        {title: 'Staples Center', venue_id: '42893400f964a5207e231fe3', location: {lat: 34.043006, lng: -118.26736}},
+        {title: 'University of Southern California', venue_id: '45001adcf964a520ab381fe3', location: {lat: 34.022352, lng: -118.285117}},
+        {title: 'China Town', venue_id: '508760b5e4b0d647dab288a6', location: {lat: 34.062334, lng: -118.238331}},
+        {title: 'Pershing Square', venue_id: '4352e980f964a520c7281fe3', location: {lat: 34.048569, lng: -118.252892}},
+        {title: 'Union Station', venue_id: '45b88de0f964a520ce411fe3', location: {lat: 34.056219, lng: -118.236502}},
+        {title: 'Grand Park LA', venue_id: '4fecf601067d351381ea64fa', location: {lat: 34.056329, lng: -118.246771}},
+        {title: 'Dodger Stadium', venue_id: '40db6b00f964a5207d011fe3', location: {lat: 34.073851, lng: -118.239958}},
+        {title: 'Little Tokyo', venue_id: '41be2d00f964a520841e1fe3', location: {lat: 34.052339, lng: -118.239505}},
+        {title: 'The Broad', venue_id: '4eeb4c14b634b469c36a8c80', location: {lat: 34.054469, lng: -118.250558}},
+        {title: 'Los Angeles City Hall', venue_id: '4b5113edf964a520314127e3', location: {lat: 34.053527, lng: -118.242932}}
+    ];
 // this is the function that make a map appear
-
-function initMap(){
-    //constructor of creating a map
-    map = new google.maps.Map(document.getElementById('map'),{
-        center: {lat:34.056329,lng:-118.246771},
-        zoom: 10
+function initMap() {
+    //use constructor to create a map
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 34.056329, lng: -118.246771},
+        zoom: 13
     });
-    // create an instance of bounds
+    //create an instance of bounds
     var bounds = new google.maps.LatLngBounds();
-    //places is observable array in the viewModel
-    model.forEach(function(places){
+    //parameter places is observable array in the viewModel
+    model.forEach(function (places) {
         //addMarkers function is defined below
         addMarkers(places);
+        //make the bound extend to the farest location
         bounds.extend(places.location);
-    });
+        });
     map.fitBounds(bounds);
-};
+    };
 //this function make markers appear on the map
-function addMarkers(place){
-        var self = this;
-        var latlng = {
-                lat: place.location.lat,
-                lng: place.location.lng
-            };
-        // this.defaultMarker = makeMarkerIcon('ec630f');
-        // this.highLightedMarker = makeMarkerIcon('');
-
-        // Instance of markers
-        self.marker = new google.maps.Marker({
-            position: latlng,
-            map: map,
-            title:place.title,
-            animation: google.maps.Animation.DROP,
-            id:place.venue_id
-        });
-        // push all markers in observable array makers defined in viewModel
-        self.markers.push(self.marker);
-        //add a listener on marker
-        self.marker.addListener('click', function(){
-            console.log('it is ok here at marker click.');
-            populateInfoWindow(place);
-        });
+function addMarkers(place) {
+    var self = this;
+    var latlng = {
+            lat: place.location.lat,
+            lng: place.location.lng
+        };
+    // Instance of markers
+    self.marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: place.title,
+        animation: google.maps.Animation.DROP,
+        id: place.venue_id
+    });
+    // push all markers in observable array makers defined in viewModel
+    self.markers.push(self.marker);
+    //add a listener on marker
+    self.marker.addListener('click', function () {
+        // console.log('it is ok here at marker click.');
+        populateInfoWindow(place);
+    });
 }
-
-
-// };
-
 //build viewModel using js constructor
-function viewModel(){
+function viewModel() {
     this.markers = ko.observableArray([]);
+    this.infoWindowArray = ko.observableArray([]);
     //search bar input
     this.query = ko.observable('');
     //observable array of model
     this.places = ko.observableArray([]);
     //google street view
-    this.place_name = ko.observable();
     this.googlePhoto = ko.observable();
-    this.infoWindowArray = ko.observableArray([]);
     //foursquare infowindow
     this.venue_rating = ko.observable();
     this.venue_name = ko.observable();
     this.venue_likes = ko.observable();
     this.venue_address = ko.observable();
-
-    model.forEach(function(places) {
+    // push all places in model to an observableArray
+    model.forEach(function (places) {
         this.places.push(places);
     });
-
-    this.filterResult = ko.computed(function(){
+    this.filterResult = ko.computed(function () {
         var query = this.query();
-        if(!query){
+        if (!query) {
             showMarkers();
             return this.places();
-        }else{
-            return ko.utils.arrayFilter(model, function(item){
-                if(item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1) {
+        }else {
+            return ko.utils.arrayFilter(model, function (item){
+                if (item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1) {
                     showFilteredMarkers(item);
                     return item;
-                }else{
+                }else {
                     hideFilteredMarkers(item);
                 }
-
             });
         }
-    },this);
-
-    this.getInfoWindow = function(place){
+    }, this);
+    // this function is for the list view item to make the list clickable
+    this.getInfoWindow = function(place) {
         populateInfoWindow(place);
-        console.log('it is ok here.');
+        // console.log('it is ok here.');
     };
 };
-
 //make markers appear on map
-function showMarkers (){
+function showMarkers() {
     var self = this;
     for (var i=0; i<self.markers().length; i++){
         self.markers()[i].setVisible(true);
         self.markers()[i].setAnimation(null);
-        // self.markers()[i].setIcon(defaultIcon);
     }
 }
-
-
 //this function shows the marker of the selected item
-function showFilteredMarkers(item){
+function showFilteredMarkers(item) {
     var self = this;
-    // closeInfoWindows();
-    for(i=0; i<self.markers().length; i++){
-        if(self.markers()[i].title.toLowerCase() === item.title.toLowerCase()){
+    closeInfoWindows();
+    for (i=0; i<self.markers().length; i++){
+        if (self.markers()[i].title.toLowerCase() === item.title.toLowerCase()) {
             self.markers()[i].setVisible(true);
             self.markers()[i].setAnimation(google.maps.Animation.BOUNCE);
             // self.markers()[i].setIcon(self.highLightedIcon);
@@ -130,33 +116,30 @@ function showFilteredMarkers(item){
         }
     }
 }
-
 //this function hides markers except the selected item
-function hideFilteredMarkers(item){
+function hideFilteredMarkers(item) {
     var self = this;
     // closeInfoWindows();
-    for( i=0; i<self.markers().length; i++){
-        if(self.markers()[i].title.toLowerCase() === item.title.toLowerCase()){
+    for (i = 0; i < self.markers().length; i++) {
+        if (self.markers()[i].title.toLowerCase() === item.title.toLowerCase()) {
             self.markers()[i].setVisible(false);
             self.markers()[i].setAnimation(null);
-            // self.markers()[i].setIcon(self.defaultIcon);
         }
     }
 }
-
-function populateInfoWindow(place){
+function populateInfoWindow(place) {
     var self = this;
     var currentClickedMarker;
     //iterate markers array to see which marker is clicked
-    for( i=0; i<self.markers().length; i++){
-        if (self.markers()[i].title.toLowerCase() === place.title.toLowerCase()){
+    for (i = 0; i < self.markers().length; i++) {
+        if (self.markers()[i].title.toLowerCase() === place.title.toLowerCase()) {
             currentClickedMarker = self.markers()[i];
         }
     }
     //check the clicked marker, make it no animation after clicked
-    if(currentClickedMarker.getAnimation() !== null){
+    if (currentClickedMarker.getAnimation() !== null) {
         currentClickedMarker.setAnimation == (null);
-    } else{
+    } else {
         currentClickedMarker.setAnimation(google.maps.Animation.BOUNCE);
         setTimer(currentClickedMarker);
     }
@@ -164,13 +147,11 @@ function populateInfoWindow(place){
     var streetViewAPI = 'AIzaSyA-wuhRggL_R0VvRimv2eIzlPOk2JyAoRM';
     var lat = place.location.lat;
     var lng = place.location.lng;
-    var streetViewAPIendPoint = 'https://maps.googleapis.com/maps/api/streetview?' + 'size=400x400&location='+ lat +',' + lng + '&heading=100&pitch=10&scale=2' + '&key=' + streetViewAPI;
-
-    // Check if no image available
-    (!streetViewAPIendPoint)?
+    var streetViewAPIendPoint = 'https://maps.googleapis.com/maps/api/streetview?' + 'size=400x400&location=' + lat + ',' + lng + '&heading=100&pitch=10&scale=2' + '&key=' + streetViewAPI;
+    // Check if image available
+    (!streetViewAPIendPoint) ?
     self.googlePhoto('Sorry, no photo available') :
     self.googlePhoto(streetViewAPIendPoint);
-
     //Foursquare credentials
     var foursquareClientID = 'DENTERFPABEBP30U0SMIYWBRXNZA1DLQRMEZH3HXFBENLKXT';
     var foursquareClientSecret = 'P0UYTAX53TJI3DYTNTFZTH0EL1F2TKBAF4E1HCMXRIYNA5K4';
@@ -181,7 +162,7 @@ function populateInfoWindow(place){
         type: 'GET',
         url: foursquareAPIEndPoint,
         processData: false,
-    }).done(function(data){
+    }).done(function (data) {
         var fourSquareLocation = data.response.venue;
         var address = fourSquareLocation.location.formattedAddress;
 
@@ -192,7 +173,7 @@ function populateInfoWindow(place){
 
 
         var addressForm = '';
-        address.forEach(function(address){
+        address.forEach(function (address) {
             addressForm += address + ' ';
         });
 
@@ -202,63 +183,57 @@ function populateInfoWindow(place){
 
         //create a dom node for the infowindow
         var windowContent = '';
-
-        windowContent += '<div id= "infoWindowContent">';
-        windowContent += '<h2>'+ self.venue_name() + '</h2>';
+        windowContent += '<div id="infoWindowContent">';
+        windowContent += '<h2>' + self.venue_name() + '</h2>';
         windowContent += '<img src=' + self.googlePhoto() + '>';
-        windowContent += '<p>Address- '+ self.venue_address() +'</p>';
-        windowContent += '<p>Likes- '+ self.venue_likes() +'</p>';
-        windowContent += '<p>Rating-' + self.venue_rating() + '</p>';
+        windowContent += '<p>Address- ' + self.venue_address() +'</p>';
+        windowContent += '<p>Likes- ' + self.venue_likes() +'</p>';
+        windowContent += '<p>Rating- ' + self.venue_rating() + '</p>';
         windowContent += '</div>';
 
         //creat instance of google infowindow
         var infoWindow = new google.maps.InfoWindow({
             content: windowContent,
-            position:{lat: lat, lng: lng},
+            position: {lat: lat, lng: lng},
             maxWidth: 300
         });
-
         //push infowindows into infowindow array which is observable
         self.infoWindowArray.push(infoWindow);
         // This will open infowindow on its marker
         infoWindow.open(map);
-
         //empty the search bar list view once a marker is clicked
         self.query('');
         //close the infowindow when it's clicked again
-        infoWindow.addListener('closeclick', function(){
+        infoWindow.addListener('closeclick', function () {
             self.marker = null;
         });
 
     })
-    .fail(function(error){
-        foursquareError();
-    });
-
+        .fail(function(error) {
+            foursquareError();
+        });
 }
-
 // This function will automatically stops marker animation
 // After 3 seconds
 function setTimer(marker) {
-    setTimeout(function() {
+    setTimeout(function () {
         marker.setAnimation(null);
     }, 3000);
 }
-
 // This function close all of the infoWindows
 function closeInfoWindows() {
-    for(var i=0; i<this.infoWindowArray().length; i++) {
+    for (var i=0; i<this.infoWindowArray().length; i++) {
         this.infoWindowArray()[i].close();
     }
 }
-
+//apply knockout bindings
 ko.applyBindings(viewModel);
-
-function foursquareError(){
-    alert('Yoops, foursquare data cannot be loaded.')
+// foursquare error message
+function foursquareError() {
+    alert('Yoops, foursquare data cannot be loaded.');
 }
-
-
-
-
+// google map API error message
+function googleMapError() {
+    alert('Google map can not be loaded');
+}
 
